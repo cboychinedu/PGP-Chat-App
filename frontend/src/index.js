fcbe8f6f4a6f737b02d01cb6256d6b751f03f642
cboy.chinedu <cboy.chinedu@gmail.com>
2025-10-08 23:48:45 +0100

    Working on the frontend components

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,16 @@
+// Importing the necessary libraries and components
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+// Rendering the main App component into the root element of the HTML
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Using React's StrictMode for highlighting potential problems in the application
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
