54bb30f1d65532cb7448f9237cffbdc84d0f3544
cboy.chinedu <cboy.chinedu@gmail.com>
2025-10-08 23:52:26 +0100

    setting the necessary configurations

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// Importing the necessary modules 
+import React, { Component, Fragment } from 'react'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Defining the App component
+class App extends Component {
+  // Setting the state 
+  state = {
+    message: "Welcome to PGP!"
+  }
+
+  // Rendering the component 
+  render() {
+    // Returning the JSX to be rendered
+    return (
+      <Fragment>
+        <h1>{this.state.message}</h1>
+      </Fragment>
+    );
+  }
 }
 
+// Exporting the App component as default
 export default App;
